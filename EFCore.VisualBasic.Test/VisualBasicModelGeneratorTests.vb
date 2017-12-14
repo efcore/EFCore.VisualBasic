1b5773a6e04c6c471a3f2a0e52904ae5b6c2ceee
Brice Lambson <bricelam@users.noreply.github.com>
2017-12-14 09:23:01 -0800

    Update to latest APIs

---
+++ EFCore.VisualBasic.Test/VisualBasicModelGeneratorTests.vb
@@ -0,0 +1,41 @@
+﻿Imports Microsoft.EntityFrameworkCore
+Imports Microsoft.EntityFrameworkCore.Scaffolding
+Imports Xunit
+
+Public Class VisualBasicModelGeneratorTests
+    <Fact>
+    Public Sub Language_works()
+        Dim generator = CreateGenerator()
+
+        Assert.Equal("VB", generator.Language)
+    End Sub
+
+    <Fact>
+    Public Sub GenerateModel_works()
+        Dim generator = CreateGenerator()
+        Dim model = (New TestDbContext).Model
+
+        Dim result = generator.GenerateModel(
+            model,
+            "MyNamespace",
+            "MyDbContext",
+            "",
+            True)
+
+        Assert.Equal("MyDbContext.vb", result.ContextFile.Path)
+        Assert.NotEmpty(result.ContextFile.Code)
+        Assert.Equal(0, result.AdditionalFiles.Count)
+    End Sub
+
+    Private Function CreateGenerator() As VisualBasicModelGenerator
+        Return New VisualBasicModelGenerator(New ModelCodeGeneratorDependencies())
+    End Function
+
+    Private Class TestDbContext
+        Inherits DbContext
+
+        Protected Overrides Sub OnConfiguring(options As DbContextOptionsBuilder)
+            options.UseSqlite("Data Source=:memory:")
+        End Sub
+    End Class
+End Class
---
+++ EFCore.VisualBasic/VisualBasicModelGenerator.vb
@@ -0,0 +1,39 @@
+﻿Imports Microsoft.EntityFrameworkCore.Metadata
+Imports Microsoft.EntityFrameworkCore.Metadata.Internal
+Imports Microsoft.EntityFrameworkCore.Scaffolding
+
+Public Class VisualBasicModelGenerator
+    Inherits ModelCodeGenerator
+
+    Public Sub New(dependencies As ModelCodeGeneratorDependencies)
+        MyBase.New(dependencies)
+    End Sub
+
+    Public Overrides ReadOnly Property Language As String
+        Get
+            Return "VB"
+        End Get
+    End Property
+
+    Public Overrides Function GenerateModel(
+        model As IModel,
+        [namespace] As String,
+        contextName As String,
+        connectionString As String,
+        dataAnnotations As Boolean) As ScaffoldedModel
+
+        Dim files As New ScaffoldedModel
+        files.ContextFile = New ScaffoldedFile
+        files.ContextFile.Path = contextName + ".vb"
+        files.ContextFile.Code = "' TODO: Generate DbContext"
+
+        For Each entityType In model.GetEntityTypes()
+            Dim file As New ScaffoldedFile
+            file.Path = entityType.DisplayName() + ".vb"
+            file.Code = "' TODO: Generate " + entityType.DisplayName()
+            files.AdditionalFiles.Add(file)
+        Next
+
+        Return files
+    End Function
+End Class

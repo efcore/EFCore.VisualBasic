011510195d393e0d8a79b39f5f4a0ee7bd187ad8
Brice Lambson <bricelam@users.noreply.github.com>
2018-03-08 09:11:45 -0800

    Add a sandbox

---
+++ Sandbox/Migrations/20180308171022_InitialCreate.vb
@@ -0,0 +1,27 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports Microsoft.EntityFrameworkCore.Migrations
+
+Namespace Sandbox.Migrations
+    Public Partial Class InitialCreate
+        Inherits Migration
+
+        Protected Overrides Sub Up(migrationBuilder As MigrationBuilder)
+            migrationBuilder.CreateTable(
+                name:= "Blogs",
+                columns:= Function(table) New With
+                {
+                    .Id = table.Column(Of Integer)(nullable:= False) _
+                        .Annotation("Sqlite:Autoincrement", True)
+                },
+                constraints:= Sub(table)
+                    table.PrimaryKey("PK_Blogs", Function(x) x.Id)
+                End Sub)
+        End Sub
+
+        Protected Overrides Sub Down(migrationBuilder As MigrationBuilder)
+            migrationBuilder.DropTable(
+                name:= "Blogs")
+        End Sub
+    End Class
+End Namespace

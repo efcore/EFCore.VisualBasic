afaea5c1e753ff6f5c7bf1e4caf49300024da2e7
Brice Lambson <bricelam@users.noreply.github.com>
2018-03-08 09:51:16 -0800

    Add more magic

--- Sandbox/Magic.vb
+++ Sandbox/Magic.vb
@@ -1,3 +1,12 @@
 ﻿Imports Microsoft.EntityFrameworkCore.Design
+Imports Microsoft.Extensions.DependencyInjection
 
 <Assembly: DesignTimeServicesReference("Bricelam.EntityFrameworkCore.VisualBasic.EFCoreVisualBasicServices, Bricelam.EntityFrameworkCore.VisualBasic")>
+
+Class DesignTimeServices
+    Implements IDesignTimeServices
+
+    Public Sub ConfigureDesignTimeServices(serviceCollection As IServiceCollection) Implements IDesignTimeServices.ConfigureDesignTimeServices
+        SQLitePCL.Batteries_V2.Init()
+    End Sub
+End Class

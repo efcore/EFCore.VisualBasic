eaa8b48822bbc258b7aa3bf90bc1527e823ffca2
lorcQc <l_orc@hotmail.com>
2021-08-09 15:44:44 -0400

    Delete the files from the old SourceGenerator project

--- EFCore.VisualBasic.CompiledModels/Providers/IProvider.vb
+++
@@ -1,6 +0,0 @@
-﻿Friend Interface IProvider
-    ReadOnly Property ForProvider As String
-
-    Function GetDesignTimeServices(currentAssemblyName As String) As String
-    Function GetRuntimeAnnotationCodeGenerator() As String
-End Interface

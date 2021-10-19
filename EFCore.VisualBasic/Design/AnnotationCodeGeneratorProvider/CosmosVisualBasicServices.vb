8b9379f7ba1818e8055f0f96bf9673187ad04dcb
lorcQc <34991704+lorcQc@users.noreply.github.com>
2021-10-19 19:18:17 -0400

    Fix the generated source file
    * Fix #63

--- EFCore.VisualBasic/Design/AnnotationCodeGeneratorProvider/CosmosVisualBasicServices.vb
+++ EFCore.VisualBasic/Design/AnnotationCodeGeneratorProvider/CosmosVisualBasicServices.vb
@@ -2,11 +2,9 @@
 Imports Microsoft.Extensions.DependencyInjection
 
 Namespace Design.AnnotationCodeGeneratorProvider
-    <VisualBasicDesignTimeProviderServices(CosmosVisualBasicServices.ProviderName)>
+    <VisualBasicDesignTimeProviderServices("Microsoft.EntityFrameworkCore.Cosmos")>
     Public Class CosmosVisualBasicServices
         Implements IDesignTimeServices
-
-        Public Const ProviderName = "Microsoft.EntityFrameworkCore.Cosmos"
 
         Public Sub ConfigureDesignTimeServices(services As IServiceCollection) Implements IDesignTimeServices.ConfigureDesignTimeServices
             services.AddSingleton(Of IVisualBasicRuntimeAnnotationCodeGenerator, CosmosVisualBasicRuntimeAnnotationCodeGenerator)
--- EFCore.VisualBasic/Design/AnnotationCodeGeneratorProvider/InMemoryVisualBasicServices.vb
+++ EFCore.VisualBasic/Design/AnnotationCodeGeneratorProvider/InMemoryVisualBasicServices.vb
@@ -2,11 +2,9 @@
 Imports Microsoft.Extensions.DependencyInjection
 
 Namespace Design.AnnotationCodeGeneratorProvider
-    <VisualBasicDesignTimeProviderServices(InMemoryVisualBasicServices.ProviderName)>
+    <VisualBasicDesignTimeProviderServices("Microsoft.EntityFrameworkCore.InMemory")>
     Public Class InMemoryVisualBasicServices
         Implements IDesignTimeServices
-
-        Public Const ProviderName = "Microsoft.EntityFrameworkCore.InMemory"
 
         Public Sub ConfigureDesignTimeServices(services As IServiceCollection) Implements IDesignTimeServices.ConfigureDesignTimeServices
             services.AddSingleton(Of IVisualBasicRuntimeAnnotationCodeGenerator, InMemoryVisualBasicRuntimeAnnotationCodeGenerator)
